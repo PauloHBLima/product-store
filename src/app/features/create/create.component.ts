9f79d5c13b610510ecc0e516a81a14b4ceb7eebb
Paulo H Lima <102702811+PauloHBLima@users.noreply.github.com>
2024-07-13 10:48:44 -0300

    feat: adiciona funcionalidade de salvar produto

--- src/app/features/create/create.component.ts
+++ src/app/features/create/create.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { ProductsService } from '../../services/products.service';
 
 @Component({
   selector: 'app-create',
@@ -16,6 +17,8 @@ import { MatInputModule } from '@angular/material/input';
   styleUrl: './create.component.scss'
 })
 export class CreateComponent {
+  productsService = inject(ProductsService);
+
   form = new FormGroup({
     title: new FormControl<string>('', {
       nonNullable: true,
@@ -24,6 +27,11 @@ export class CreateComponent {
   })
 
   onSubmit() {
-    this.form.controls.title.value;
+    this.productsService.post({
+      title: this.form.controls.title.value
+    })
+    .subscribe(()=> {
+      alert('Sucesso');
+    })
   }
 }
