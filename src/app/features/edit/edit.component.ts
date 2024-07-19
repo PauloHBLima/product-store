78a62bd37b8b2b85b206a302b98f644b96de4dbb
Paulo H Lima <102702811+PauloHBLima@users.noreply.github.com>
2024-07-18 21:17:38 -0300

    feath: componente de edição

--- src/app/features/edit/edit.component.ts
+++ src/app/features/edit/edit.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { ProductsService } from '../../services/products.service';
 
 @Component({
   selector: 'app-edit',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
   templateUrl: './edit.component.html',
   styleUrl: './edit.component.scss'
 })
 export class EditComponent {
 
+  productsService = inject(ProductsService);
+  matSnackBar = inject(MatSnackBar);
+  router  = inject(Router);
+
+
+
+  form = new FormGroup({
+    title: new FormControl<string>('', {
+      nonNullable: true,
+      validators: Validators.required,
+    })
+  })
+
+  onSubmit() {
+
+    this.productsService.post({
+      title: this.form.controls.title.value
+    })
+      .subscribe(() => {
+        this.matSnackBar.open('Produto criado com sucesso!', 'Ok');
+
+        this.router.navigateByUrl('/');
+      })
+}
 }
